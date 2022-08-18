301a7b24cf8232f3274082425efaaa273a48b4f3
Sebastiaan Huber <mail@sphuber.net>
2022-08-18 10:46:21 +0200

    CLI: Add command `analyse cif-clean-manual`
    This command prints a table with the number of successfully completed
    clean work chains that were later found to actually have produced a
    `StructureData` that is incompatible with the `CifData` due to the
    chemical formulas being incompatible.

--- aiida_sdb/cli/analyse/cif_clean.py
+++ aiida_sdb/cli/analyse/cif_clean.py
@@ -67,3 +67,54 @@ def cmd_cif_clean(database, fmt):
         table.append(row)
 
     echo.echo(tabulate.tabulate(table, headers=headers, tablefmt=fmt))
+
+
+@cmd_analyse.command('cif-clean-manual')
+@click.option(
+    '-f',
+    '--format',
+    'fmt',
+    type=click.Choice(['plain', 'latex']),
+    default='plain',
+    help='Format to use for printing the final table.'
+)
+def cmd_cif_clean_manual(fmt):
+    """Print table with successfully completed workchains that had invalid ``CifData`` as input that went overlooked.
+
+    After the work chains had finished it was discovered that many of the ``CifData`` that were input to a work chain
+    that finished successfully were actually inconsistent. A separate analysis compared the input ``CifData`` with the
+    parsed ``StructureData`` for their structural formula and found various problems. The affected ``CifData`` nodes,
+    both source and cleaned version, as well as the parsed ``StructureData`` were tagged with the ``incorrect_formula``
+    extra, that takes the value ``missing_hydrogen``, ``missing_lithium``, ``missing_other`` and ``different_comp``.
+
+    This command will print the numbers of these that are found in each database.
+    """
+    from aiida import orm
+
+    counters = []
+
+    for db in DATABASES:
+
+        filters = {'extras': {'has_key': 'incorrect_formula'}}
+
+        builder = orm.QueryBuilder()
+        builder.append(orm.Group, filters={'label': f'{db}/workchain/clean'}, tag='group')
+        builder.append(orm.WorkChainNode, with_group='group', tag='wc', filters={'attributes.exit_status': {'==': 0}})
+        builder.append(orm.CifData, with_incoming='wc', filters=filters, project='extras.incorrect_formula')
+
+        counter = collections.Counter(builder.all(flat=True))
+        counters.append(counter)
+
+    table = []
+    headers = ['Error']
+    headers.extend(DATABASES)
+
+    keys = sorted(set(key for counter in counters for key in counter.keys()))
+
+    for key in keys:
+
+        row = [key]
+        row.extend([counter.get(key) for counter in counters])
+        table.append(row)
+
+    echo.echo(tabulate.tabulate(table, headers=headers, tablefmt=fmt))
